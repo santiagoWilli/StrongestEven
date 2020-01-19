fd468dc991296cb43778bae2ad42d8091460b9b6
santiagoWilli <santiago30mw@gmail.com>
2020-01-19 19:54:45 +0000

    Fixed misconception

--- src/main/java/StrongestEven.java
+++ src/main/java/StrongestEven.java
@@ -2,7 +2,7 @@ public class StrongestEven {
 
     public static Integer of(int[] interval) {
         if (interval[0] >= interval[1]) throw new IllegalIntervalException();
-        return (interval[0] == 1 ? 1 : 2);
+        return (interval[0] == 1 ? 2 : 4);
     }
 
     static class IllegalIntervalException extends RuntimeException {}
