6b0c256368aa76f23c437156b5b7bf50b5472383
santiagoWilli <santiago30mw@gmail.com>
2020-01-19 19:40:23 +0000

    StrongestEven class which calls at StrongInteger Wrong interval returns null

---
+++ src/main/java/StrongestEven.java
@@ -0,0 +1,8 @@
+public class StrongestEven {
+
+    public static Integer of(int[] interval) {
+        throw new IllegalIntervalException();
+    }
+
+    static class IllegalIntervalException extends RuntimeException {}
+}
---
+++ src/test/java/StrongestEven_.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(Parameterized.class)
+public class StrongestEven_ {
+    private final int[] interval;
+    private final Integer strongestEven;
+    private final Class exception;
+
+    public StrongestEven_(int[] interval, Integer strongestEven, Class exception) {
+        this.interval = interval;
+        this.strongestEven = strongestEven;
+        this.exception = exception;
+    }
+
+    @Test
+    public void execute() {
+        try {
+            assertThat(StrongestEven.of(interval)).isEqualTo(strongestEven);
+            assertThat(exception).isEqualTo(null);
+        } catch (Exception e) {
+            assertThat(exception).isNotEqualTo(null);
+        }
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] cases() {
+        return new Object[][]{
+                {new int[]{4, 2}, null, StrongestEven.IllegalIntervalException.class},
+        };
+    }
+}
